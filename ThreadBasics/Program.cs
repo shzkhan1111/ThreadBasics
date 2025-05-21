532e8c58ef04c756a729d312960a378a387e6ee9
Shahzaib Ahmed Khan <shahzaib.ahmed@hyperdatacomputing.com>
2025-05-21 16:29:04 +0500

    Lecture 1: Thread Priorities

--- ThreadBasics/Program.cs
+++ ThreadBasics/Program.cs
@@ -3,25 +3,30 @@
     for (int i = 0; i < 100; i++)
     {
         Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-        Thread.Sleep(10); // Sleep for 10 milliseconds
+        //Thread.Sleep(10); // Sleep for 10 milliseconds
     }
 }
 #region blocking thread
 //#1
-WriteThreadId();//in the main thread is blocking will be printed first must be completed before child thread start
+//WriteThreadId();//in the main thread is blocking will be printed first must be completed before child thread start
 //start another thread use thread class
 //takes a delegate
 #endregion
-Thread thread1  = new Thread(WriteThreadId); // will write a different value as compared to the first one #1
+Thread thread1  = new Thread(WriteThreadId); 
+Thread thread2 = new Thread(WriteThreadId);
+
+thread1.Priority = ThreadPriority.Highest;
+thread2.Priority = ThreadPriority.Lowest;
+
+//make the current priority normal
+Thread.CurrentThread.Priority = ThreadPriority.Normal;
+
 thread1.Start();
-
-
-Thread thread2 = new Thread(WriteThreadId); 
 thread2.Start();
-
-// may switch from thread1 to thread 2 based on the thread scheduler
 
 
 #region non blocking thread
 WriteThreadId(); // changing the placement will make it non blocking running it second 
+
+//before WriteThreadId() in the main thread is funished next line wont be executed
 #endregion
