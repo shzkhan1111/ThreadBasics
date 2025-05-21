00d92fefca1eac23c682c67be5bf3f8c77fb2a37
Shahzaib Ahmed Khan <shahzaib.ahmed@hyperdatacomputing.com>
2025-05-21 16:06:58 +0500

    Add project files.

---
+++ ThreadBasics/Program.cs
@@ -0,0 +1,11 @@
+﻿void WriteThreadId()
+{
+    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+}
+
+WriteThreadId();
+//start another thread use thread class
+//takes a delegate
+
+Thread thread1  = new Thread(WriteThreadId);
+thread1.Start();
