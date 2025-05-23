79343c227a87734e75c08aced0d91da02212dbcd
Shahzaib Ahmed Khan <shahzaib.ahmed@hyperdatacomputing.com>
2025-05-23 19:04:04 +0500

    Lecture 4 basic solution

--- ThreadBasics/Program.cs
+++ ThreadBasics/Program.cs
@@ -1,19 +1,53 @@
 ﻿Console.WriteLine("Simulating Server Request ");
 
+//implementr queue 
+//monitor
+//Request processor
+/*
+ in the given case input wont be blocked 
+you enter text 
+its entered into the request queue
+the monitorThread takes it out of the queue 
+and processes it
+ */
+Queue<string> requestQueue = new Queue<string>();
+
+Thread monitorThread = new Thread(MonitorQueue);
+monitorThread.Start();
+
+
 
 while (true)
 {
     string input = Console.ReadLine();
     if (input == "exit")
     {
+        Console.WriteLine("Exiting");
+
         break;
     }
-    SimulateServerRequest(input);
+    //SimulateServerRequest(input);
+    requestQueue.Enqueue(input);
+
 }
 
+//worker thead can cause race condition
+ void MonitorQueue()
+{
+    while (true)
+    {
+        if (requestQueue.Count > 0)
+        {
+            string input = requestQueue.Dequeue();
+            Thread thread = new Thread(() => SimulateServerRequest(input));
+            thread.Start();
+        }
+        Thread.Sleep(1000);
+    }
+}
 
 static void SimulateServerRequest(string input)
 {
-    Thread.Sleep(2000);
-    Console.WriteLine("Server Request Completed");
+    Thread.Sleep(1000);
+    Console.WriteLine($"Server Request {input} Completed");
 }
