3619ab422f1937a7a9b4769d1d57cc5ffdfc8036
Shahzaib Ahmed Khan <shahzaib.ahmed@hyperdatacomputing.com>
2025-05-26 18:18:58 +0500

    Manual Reset

--- ThreadBasics/Program.cs
+++ ThreadBasics/Program.cs
@@ -1,51 +1,29 @@
-﻿using AutoResetEvent autoResetEvent = new AutoResetEvent(false); //false I want the sign to be turned off initially 
+﻿using ManualResetEventSlim manualResetEvent = new ManualResetEventSlim(false);
 
-//dispose unmanaged resources 
+//manualResetEvent.Set();//turn on the signal 
+//manualResetEvent.Reset();//turn off the signal
 
-////consumeer will call Autoreset waitone  I am waiting for something 
 
-//autoResetEvent.WaitOne();
+//multiple threads released at the same time 
 
-////producer will say I have something for you 
-//autoResetEvent.Set();//set turn on the signal 
-////once consumer consumes the signal is turned off
-
-string userInput = null;
-Console.WriteLine("Write a for worker thread to start");
-//Thread thread = new Thread(Worker);
-//thread.Start(); 
-
-//crating 3 worker thread
-for (int i = 0; i < 3; i++) 
+for (int i = 0; i < 3; i++)
 {
-    Thread thread = new Thread(Worker);
-    thread.Name = $"Worker {i + 1}";
+    Thread thread = new Thread(Work);
+    thread.Name = $"Thread {i}";
     thread.Start();
 }
 
-while (true)
+Console.WriteLine("To Release All Threads Press Enter");
+Console.ReadLine();
+manualResetEvent.Set();// give signal from producer to proceed like npm i
+Console.ReadLine();
+
+void Work()
 {
-    userInput = Console.ReadLine();
-    if (userInput == "a")
-    {
-        //signal worker thread to start consuming 
-        autoResetEvent.Set();
-    }
-}
-
-
-//worker thread process 
-void Worker()
-{
-    while (true)
-    {
-        Console.WriteLine($"{Thread.CurrentThread.Name} is waiting");
-        autoResetEvent.WaitOne();
-        //when I recieve  the signal I will proceed with my task 
-
-        Console.WriteLine($"{Thread.CurrentThread.Name} proceeds");
-        Thread.Sleep(3000);
-        //after 2 second it will wait for another signal 
-
-    }
+    Console.WriteLine("Waiting for signal...");
+    manualResetEvent.Wait(); // Wait until the signal is set
+    //in auto reset as soon as it recieves the signal it resets 
+    //since reset is manullly all threads can enter
+    Thread.Sleep(1000);
+    Console.WriteLine("Signal received, continuing work...");
 }
